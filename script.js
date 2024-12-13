6d230443f321726f1bcf223a2e178657961abae1
Raj Patel <raj.patel514@gmail.com>
2024-12-12 22:35:32 -0500

    Created base class. ProductProperties

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//Part 1 - Setting up classes
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
