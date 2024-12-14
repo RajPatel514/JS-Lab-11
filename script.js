92313bc149b15f2c0345b9cd04e3f634c0256bc1
Raj Patel <raj.patel514@gmail.com>
2024-12-14 02:47:58 -0500

    Ran prettier to format JS

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //Part 1 - Setting up classes
 class ProductProperties {
     constructor(name, price, quantity) {
@@ -6,25 +5,21 @@ class ProductProperties {
         this.price = price;
         this.quantity = quantity;
     }
-
     getTotalValue() {
         return this.price * this.quantity;
     }
-
     toString() {
-        return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`
+        return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`;
     }
-
     //Part 3 - Static Methods & Properties
     //Static method to loop through index and apple discount rate to each index item
     static applyDiscount(products, discount) {
         for (let i = 0; i < products.length; i++) {
             let product = products[i];
-            product.price = product.price - (product.price * discount);
+            product.price = product.price - product.price * discount;
         }
         return products;
     }
-
 }
 
 //Part 2 - Adding inheritance
@@ -33,11 +28,9 @@ class PerishableProductProperties extends ProductProperties {
         super(name, price, quantity);
         this.expirationDate = expirationDate;
     }
-
     toString() {
         return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
     }
-
 }
 
 let myChicken = new PerishableProductProperties("Chicken", 10, 5, "Jan-14");
@@ -47,32 +40,27 @@ let myOrange = new PerishableProductProperties("Orange", 3, 7, "Jan-3");
 let myBattery = new ProductProperties("Battery", 8, 12);
 let myOil = new ProductProperties("Oil", 11, 2);
 
-
 //Part 3 Array of products + Output
 let products = [myChicken, myPeppers, myBanana, myOrange, myBattery, myOil];
 console.log(PerishableProductProperties.applyDiscount(products, 0.15));
-
 
 //Part 4 - Store Management
 class Store {
     constructor(inventory) {
         this.inventory = inventory;
     }
-
     addProduct(product) {
         this.inventory.push(product);
         console.log("Product was added to inventory");
     }
-
     getInventoryValue() {
         //method to loop through the array and add up all the price values
         let totalValue = 0;
         for (let i = 0; i < this.inventory.length; i++) {
             totalValue = this.inventory[i].price * this.inventory[i].quantity;
         }
-        return totalValue
+        return totalValue;
     }
-
     findProductByName(name) {
         //Method to loop through array and look for matching names, else null
         for (let i = 0; i < this.inventory.length; i++) {
@@ -83,13 +71,11 @@ class Store {
             }
         }
     }
-
 }
 
 //Part 5 - Testing the System
-//let myStore = new Store(products);
-
-let myStore = new Store;
+//let myStore = new Store(products); // Old store
+let myStore = new Store();
 myStore.addProduct(myChicken);
 myStore.addProduct(myBanana);
 myStore.addProduct(myBattery);
